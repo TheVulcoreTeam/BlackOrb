0fbf7430b62dc3f4413f547ac5deae8e7a0a3f41
Matías Valentín Muñoz Espinoza <writkas2@gmail.com.com>
2021-12-20 23:47:13 -0300

    WIP: Movement

--- Core/GPlayer/GPlayer.gd
+++ Core/GPlayer/GPlayer.gd
@@ -3,21 +3,26 @@ extends Node2D
 class_name GPlayer
 
 var input_vec := Vector2.ZERO
+var dest
 
 func _input(event):
 	input_vec.x = int(event.is_action_pressed("ui_right")) - int(event.is_action_pressed("ui_left")) 
-	input_vec.y = int(event.is_action_pressed("ui_bottom")) - int(event.is_action_pressed("ui_top"))
+	input_vec.y = int(event.is_action_pressed("ui_down")) - int(event.is_action_pressed("ui_up"))
 	
-	$Movement.interpolate_property(
-		self,
-		"global_position",
-		global_position,
-		(global_position + input_vec) * 8,
-		0.4,
-		Tween.TRANS_CIRC,
-		Tween.EASE_IN_OUT,
-		0.1
-	)
-	$Movement.start()
+	if input_vec != Vector2.ZERO:
+		dest = global_position + global_position.direction_to(input_vec).normalized() + Vector2(16, 16)
+		print(dest)
+		
+		$Movement.interpolate_property(
+			self,
+			"global_position",
+			global_position,
+			dest,
+			.4,
+			Tween.TRANS_SINE,
+			Tween.EASE_IN_OUT,
+			0.1
+		)
+		$Movement.start()
 	
-	
+	input_vec = Vector2.ZERO
