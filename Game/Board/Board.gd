aa209aec7c26cecf840fec64b5073b5ecf7df680
Pablo André Vasquez Romualdo <thekingofshadows98@gmail.com>
2021-12-20 20:30:39 -0600

    Adding a random Places from fire and regen places

--- Game/Board/Board.gd
+++ Game/Board/Board.gd
@@ -3,8 +3,9 @@ extends TileMap
 const BOARD_SIZE = Vector2(16, 16)
 
 var rec_regen = preload("res://Game/Places/Regen/Regen.tscn")
+var rec_fire = preload("res://Game/Places/Fire/Fire.tscn");
 var rec_player = preload("res://Game/Players/Soldier/Soldier.tscn")
-
+var free_cells = range(BOARD_SIZE.x * BOARD_SIZE.y)
 
 func _ready():
 	create_map()
@@ -12,12 +13,27 @@ func _ready():
 
 func create_map():
 	randomize()
-	create_place(Vector2(randi() % 16, randi() % 16), rec_regen.instance())
-	create_place(Vector2(randi() % 16, randi() % 16), rec_regen.instance())
-	create_place(Vector2(randi() % 16, randi() % 16), rec_regen.instance())
+	create_player(_get_free_rand_cell(true), rec_player.instance())
+	# Generar los places especiales de manera aleatoria
+	_generate_places( 150 , rec_regen)
+	_generate_places( 150 , rec_fire)
 	
-	create_player(Vector2(randi() % 16, randi() % 16), rec_player.instance())
 
+func _generate_places( cant , place):
+	
+	for a in range(cant):
+		if len(free_cells) > 0:
+			var pos = _get_free_rand_cell(true)
+			create_place(pos, place.instance())			
+		else: break
+
+func _get_free_rand_cell(removecell: bool):
+	var i = randi() % len(free_cells)
+	var cell = free_cells[i];
+	var _x = cell % int(BOARD_SIZE.x)
+	var _y = floor(cell / BOARD_SIZE.x)
+	if(removecell): free_cells.remove(i);
+	return Vector2(_x,_y)
 
 func create_place(cell_pos : Vector2, object : GPlace):
 	var obj_pos = _get_pos_in_board(cell_pos)
