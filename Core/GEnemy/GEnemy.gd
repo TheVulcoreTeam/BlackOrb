8da51ea5a83b355ffb39a9570f8118869a5a7cf2
Pablo André Vasquez Romualdo <thekingofshadows98@gmail.com>
2021-12-20 21:54:25 -0600

    fixed Dark Soldier :c

--- Core/GEnemy/GEnemy.gd
+++ Core/GEnemy/GEnemy.gd
@@ -4,6 +4,7 @@ class_name GEnemy
 
 var board_position
 
+
 func _ready():
 	Signals.connect("end_turn",self, "on_cpu_turn")
 	pass
@@ -17,8 +18,16 @@ func _move_to_player():
 	randomize()
 	var pos = board_position
 	var movimiento = Vector2((randi() % 3) - 1 , (randi() % 3) - 1 )
-	pos = Vector2(clamp(pos.x + movimiento.x, 0, 15),clamp(pos.y + movimiento.y, 0, 15) )
+	
+	pos = Vector2(pos.x + movimiento.x,pos.y + movimiento.y)
+	if(pos.x < 0): pos.x = 0
+	if(pos.y < 0): pos.y = 0
+	if(pos.x > 15): pos.x = 15
+	if(pos.y > 15): pos.y = 15
+	var dest = pos - board_position
+	self.global_position += (dest) * 16
 	board_position = pos
+	print(board_position)
 	#self.global_position = Board._get_pos_in_board(pos)
 	Turns.next_turn()
 	pass
